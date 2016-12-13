df503bd2e1556379f8cd1a36aad5d094bffbdc60
Jacob Morris <blendingjake@gmail.com>
2016-12-13 10:55:41 -0500

    made version 0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name="pytimer",
-      version="1.1",
+      version="0.2",
       py_modules=["pytimer"],
       author="Jacob Morris",
       author_email="blendingjake@gmail.com")
